f22dd13adef152e7c4d946073cf94f75ae508ebd
Ramsey Njire <ramseynjire@gmail.com>
2021-03-25 12:19:18 +0300

    add associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ApplicationRecord
+
+    has_many(
+        :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :course_id,
+        primary_key: :id
+    )
+    
+    has_many :enrolled_students, through: :enrollments, source: :user
+
 end
