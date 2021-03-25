4e9027df78fd28b96c047c125378fd3c369589fd
Ramsey Njire <ramseynjire@gmail.com>
2021-03-25 12:47:55 +0300

    clean up associations for prequisites and instructors

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,7 +16,7 @@ class Course < ApplicationRecord
 
     belongs_to(
         :instructor,
-        class_name: :user,
+        class_name: 'User',
         foreign_key: :instructor_id,
         primary_key: :id
     )
